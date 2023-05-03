373b67a09da8a3985840d01ebb88429840307bc1
raaz1212 <araj30728@gmail.com>
2023-05-04 05:24:40 +0600

    first commit

---
+++ src/components/layouts/Main.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Main;
